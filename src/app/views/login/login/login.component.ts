f55a2d5d3b8ec246f75aceab6e71b139685ffd7e
pidp1 <pedro.imparato@gmail.com>
2022-12-01 15:11:09 -0300

    commit inicial

---
+++ src/app/views/login/login/login.component.ts
@@ -0,0 +1,43 @@
+import { Credenciais } from './../../../models/credenciais';
+import { AuthService } from './../../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public formLogin: FormGroup;
+
+  constructor(
+    formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.formLogin = formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      senha: ["", [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  public signIn(): void {
+    if(this.formLogin.valid) {
+      // PROCESSO DE AUTENTICAR
+      const credenciais: Credenciais = this.formLogin.value;
+      this.authService.authenticate(credenciais).subscribe(response => {
+        alert("Bem-vindo(a)!");
+        this.router.navigate(["/home"]);
+      });
+    }
+    else {
+      alert("Dados inválidos.");
+    }
+  }
+}
